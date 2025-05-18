456d204dcee4944d33578c268ca9300791218d1c
salim <lesalgeriensdesstars@gmail.com>
2025-05-18 10:53:13 +0100

    still not working :P

--- ollama_api.py
+++ ollama_api.py
@@ -1,11 +1,19 @@
 import requests
-import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 llm_model = "deepseek-r1:8b"
-ollama_api_url = "localhost"
+ollama_api_url = "10.0.3.230"
 port = 11434
 
-def ollama_post_(data: dict, suffix: str, url: str = ollama_api_url, port: int = port):
+
+def ollama_post_(
+        data:   dict,
+        suffix: str,
+        url:    str = ollama_api_url,
+        port:   int = port
+        ):
+
     _url = f"http://{url}:{port}/{suffix}"
     response = requests.post(url=_url, json=data)
     if response.status_code == 200:
@@ -14,6 +22,7 @@ def ollama_post_(data: dict, suffix: str, url: str = ollama_api_url, port: int =
         print("request failed:", response.text)
         return None
 
+
 def invoke(
         prompt:		str,
         suffix:		str = "",
@@ -21,6 +30,7 @@ def invoke(
         system:		str = "",
         template:	str = "",
         options:	dict | None = None,
+        # how do i insert context
         context:	list | None = None,
         keep_alive:	str = "5m",
         stream:		bool = False,
@@ -38,3 +48,28 @@ def invoke(
         "stream": stream,
         "keep_alive": keep_alive,
         }, "api/generate").json()
+
+
+import json
+import os
+
+
+def generate(prompt:str):
+
+    if os.path.exists(".context"):
+        print("bech tchouf")
+        file = open(mode="r", file=".context")
+        context = '{"context":['+file.read()+']}'
+        file.close()
+    else:
+        context = '{"context":[]}'
+
+
+    response = invoke(prompt, context = json.loads(context)["context"])
+
+    file = open(mode="w", file=".context")
+    file.write(f"{response['context']}")
+    file.close()
+
+    return response
+

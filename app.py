283c811845a0fcb3efbf6993a5f115c6a5d3c3c9
salim <lesalgeriensdesstars@gmail.com>
2025-05-22 12:05:25 +0100

    aarrrghhhh

--- app.py
+++ app.py
@@ -4,45 +4,31 @@ import argparse
 from pprint import pprint
 
 def main():
-    # Initialize the parser
     parser = argparse.ArgumentParser(prog="prog")
 
-    # Define subparsers for the commands
     subparsers = parser.add_subparsers(dest="command")
 
-    # Subcommand: list
-    list_parser = subparsers.add_parser('list', help='List agents or PCs')
+    list_parser = subparsers.add_parser('list')
     list_subparsers = list_parser.add_subparsers(dest="list_type")
-    list_subparsers.add_parser('agents', help='List all agents')
-    # list_subparsers.add_parser('policyChecks', help='List all PCs')
-    # list_subparsers.add_parser('scaDatabase', help='List all PCs')
 
-    # # Subcommand: gen
-    # gen_parser = subparsers.add_parser('generate', help='')
-    # gen_subparsers = gen_parser.add_subparsers(dest="gen_type")
-    # gen_subparsers.add_parser('pc', help='')
-    # gen_subparsers.add_parser('agents', help='')
+    list_subparsers.add_parser('agents')
 
-    # Parse the arguments
+    policy_checks_parser = list_subparsers.add_parser('policyChecks')
+    policy_checks_parser.add_argument('arg1', type=str)
+    policy_checks_parser.add_argument('arg2', type=str)
+
+
     args = parser.parse_args()
 
-    # Handle the parsed arguments
     if args.command == 'list':
         if args.list_type == 'agents':
             pprint(wazuh_api.get_agents())
-        # elif args.list_type == 'policyChecks':
-        #     pass
-    #     else:
-    #         print("Invalid list type")
-    # elif args.command == 'gen':
-    #     if args.gen_type == 'pc':
-    #         pass
-    #     elif args.gen_type == 'agents':
-    #         pass
-    #     else:
-    #         print("Invalid gen type")
-    # else:
-    #     print("Invalid command")
+            print("")
+        elif args.list_type == 'policyChecks':
+            result = wazuh_api.get_policy_checks(args.arg1, args.arg2, select="id,title,result")
+            for policy in result:
+                print(f"{policy['id']} [{policy['result']}] {policy['title']}")
+            print("")
 
 if __name__ == "__main__":
     main()
